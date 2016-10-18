ed2a130f3397c071683df61b0db9c8bf77074405
Kevin Saliou <kevin@saliou.name>
2016-10-18 15:42:45 +0200

    implement user::create & update

--- src/TrustFully/Api/User.php
+++ src/TrustFully/Api/User.php
@@ -5,4 +5,55 @@ namespace TrustFully\Api;
 class User extends AbstractApi
 {
     protected $endPoint = 'users';
+
+    /**
+     * @param string $email
+     * @param string $plainPassword
+     * @param array  $extraData
+     *
+     * @return array
+     */
+    public function create($email, $plainPassword, array $extraData = array())
+    {
+        $defaults = array(
+            'email' => $email,
+            'plainPassword' => $plainPassword,
+            'gender' => null,
+            'greeting' => null,
+            'firstname' => null,
+            'lastname' => null,
+            'city' => null,
+            'state' => null,
+            'countryCode' => null,
+        );
+        $params = $this->sanitizeParams($defaults, $extraData);
+        $json = $this->client->post(sprintf('/%s', $this->endPoint), $params);
+
+        return $this->client->decode($json);
+    }
+
+    /**
+     * @param string $id
+     * @param array  $params
+     *
+     * @return array
+     */
+    public function update($id, array $params = array())
+    {
+        $defaults = array(
+            'email' => null,
+            'plainPassword' => null,
+            'gender' => null,
+            'greeting' => null,
+            'firstname' => null,
+            'lastname' => null,
+            'city' => null,
+            'state' => null,
+            'countryCode' => null,
+        );
+        $params = $this->sanitizeParams($defaults, $params);
+        $json = $this->client->put(sprintf('/%s/%s', $this->endPoint, $id), $params);
+
+        return $this->client->decode($json);
+    }
 }
